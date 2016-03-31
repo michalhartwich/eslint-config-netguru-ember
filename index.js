575994ce993a50e9a55924c32861144f33966931
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-03-31 14:12:39 +0200

    Initial structure

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    'airbnb/base'
+  ],
+  globals: {
+  },
+  plugins: [
+    'netguru-ember'
+  ],
+  rules: {
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'no-shadow': 0,
+    'no-else-return': 0,
+    'no-extend-native': 0,
+    'arrow-body-style': 0,
+    'prefer-arrow-callback': 0,
+    'prefer-rest-params': 0,
+    'consistent-return': 0,
+  }
+}
