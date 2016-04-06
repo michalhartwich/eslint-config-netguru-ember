d185fcaa5cfaa3d179627b331a3142450c97562b
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-04-06 09:39:29 +0200

    Turn on "no-empty-attrs" rule

--- index.js
+++ index.js
@@ -28,5 +28,6 @@ module.exports = {
     'netguru-ember/order-in-models': 2,
     'netguru-ember/alias-model-in-controller': 2,
     'netguru-ember/query-params-on-top': 2,
+    'netguru-ember/no-empty-attrs': 2,
   }
 }
